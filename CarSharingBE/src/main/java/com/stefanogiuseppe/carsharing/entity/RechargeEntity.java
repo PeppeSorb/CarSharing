dbd7f3e20e457fd9c617344b4911811c4b3a3a52
PeppeSorb <giuseppesorbello98.ct@gmail.com>
2023-08-09 14:48:05 +0200

    up

---
+++ CarSharingBE/src/main/java/com/stefanogiuseppe/carsharing/entity/RechargeEntity.java
@@ -0,0 +1,32 @@
+package com.stefanogiuseppe.carsharing.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "recharge")
+@Where(clause = "deleted = 0")
+public class RechargeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    private UserEntity idUser;
+
+    @Column(name="amount")
+    private double amount;
+
+    @Column(name="date_time")
+    private LocalDateTime dateTime;
+}
---
+++ CarSharingBE/src/main/java/com/stefanogiuseppe/carsharing/entity/RentalEntity.java
@@ -0,0 +1,41 @@
+package com.stefanogiuseppe.carsharing.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "rental")
+@Where(clause = "deleted = 0")
+public class RentalEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_user")
+    private UserEntity idUser;
+
+    //@Column(name = "id_vehicle")
+    //private VehicleEntity id_vehicle;
+
+    //@Column(name = "id_admin")
+    //private AdministratorEntity id_admin;
+
+    @Column(name = "date_time_start_rental")
+    private Date dateTimeStartRental;
+
+    @Column(name = "date_time_end_rental")
+    private Date dateTimeEndRental;
+
+    @Column(name = "type_rental")
+    private String typeRental;
+}
+
