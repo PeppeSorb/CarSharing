98ffbbd47375a5f311cf39ea3d57166899bd8672
stefadb <stefadb2@gmail.com>
2023-08-09 14:29:29 +0200

    Create ModelEntity.java

---
+++ CarSharingBE/src/main/java/com/stefanogiuseppe/carsharing/entity/ModelEntity.java
@@ -0,0 +1,44 @@
+package com.stefanogiuseppe.carsharing.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "vehicle")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Data
+//@Where(clause = "deleted = 0")
+public class ModelEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_category")
+    @Column(name = "id_category")
+    private Long idCategory;
+
+    @Column(name = "make_and_model")
+    private String makeAndModel;
+
+    @Column(name = "boot_capacity")
+    private int bootCapacity;
+
+    @Column(name = "average_consumption")
+    private double averageConsumption;
+
+    @Column(name = "for_new_drivers")
+    private boolean forNewDrivers;
+}
