0e28d09a4ddd722b9bb466e641a6bfc74ec64f2f
stefadb <stefadb2@gmail.com>
2023-08-09 14:05:41 +0200

    Create VehicleEntity.java

---
+++ CarSharingBE/src/main/java/com/stefanogiuseppe/carsharing/entity/VehicleEntity.java
@@ -0,0 +1,85 @@
+package com.stefanogiuseppe.carsharing.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "project")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Data
+//@Where(clause = "deleted = 0")
+public class VehicleEntity {
+    /*
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "project_type")
+    private String projectType;
+
+    @Column(name = "project_name")
+    private String projectName;
+
+    @Column(name = "presumed_starting_month")
+    private LocalDate presumedStartingMonth;
+
+    @Column(name = "presumed_end_month")
+    private LocalDate presumedEndMonth;
+
+    @Column(name = "id_allocated_resources")
+    private Long idAllocatedResources;
+
+    @Column(name = "link_dir_business_onedrive")
+    private String linkDirBusinessOnedrive;
+
+    @Column(name = "link_dir_operative_onedrive")
+    private String linkDirOperativeOnedrive;
+
+    @Column(name = "link_trello")
+    private String linkTrello;
+
+    @Column(name = "link_gitlab")
+    private String linkGitlab;
+
+    @Column(name = "link_logbook")
+    private String linkLogbook;
+
+    @ManyToOne
+    @JoinColumn(name = "id_program")
+    @JsonIgnore
+    private ProgramEntity idProgram;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idProject")
+    // Attributo nella classe Project che fa riferimento a program
+    private List<SubProjectEntity> subProjects;
+
+    @Column(name = "deleted", nullable = false, columnDefinition = "bit default 0")
+    @JsonIgnore
+    private boolean deleted;
+    */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_vehicle")
+    private Long id;
+
+    private String license_plate;
+
+    private int id_model;
+
+    
+}
