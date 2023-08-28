a02fd4bcb26d08473b6f0ce8ee7629062d408337
PeppeSorb <giuseppesorbello98.ct@gmail.com>
2023-08-28 17:22:50 +0200

    Up

---
+++ CarSharingBE/src/main/java/com/stefanogiuseppe/carsharing/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.stefanogiuseppe.carsharing.config;
+
+/*import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests((authz) -> authz
+                        //.requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/rental/create/**").fullyAuthenticated()// .hasRole("USER")
+                        //.anyRequest().authenticated()
+                       // .and().formLogin("/login")
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
+*/
