d7edb7cf15d8800cbd0c59ba023fb1069c50115e
jackJack28 <139848078+jackJack28@users.noreply.github.com>
2023-08-29 14:57:13 +0200

    Homepage edited

--- CarSharingFE/CarSharingFE/src/app/app.module.ts
+++ CarSharingFE/CarSharingFE/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { FaresComponent } from './components/fares/fares.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FaresComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
